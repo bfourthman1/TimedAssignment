e9b09ca93baca4eb92aeea0f0e239a738846108d
Mercedez Bray <mercedezbray@gmail.com>
2022-04-21 18:28:01 -0400

    database added

--- SocialCircleAPI/Migrations/20220421221345_Comment.Designer.cs
+++ SocialCircleAPI/Migrations/20220421221345_Comment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialCircle.Data;
 
@@ -11,9 +12,10 @@ using SocialCircle.Data;
 namespace SocialCircle.Migrations
 {
     [DbContext(typeof(SocialCircleDbContext))]
-    partial class SocialCircleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220421221345_Comment")]
+    partial class Comment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
